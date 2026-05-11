13f9ae121767c2a114b947dfaa2229ce21dd04ec
Asiimwe Mark <markasiimwe75@gmail.com>
2026-05-11 12:35:40 +0300

    Fix supabase configurations

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,5 +3,6 @@ import { createBrowserClient } from "@supabase/ssr";
 export const createClient = () =>
   createBrowserClient(
     (process.env.NEXT_PUBLIC_SUPABASE_URL ?? (() => { throw new Error('NEXT_PUBLIC_SUPABASE_URL is required for NEXT_PUBLIC_SUPABASE_URL'); })()),
+    (process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ?? (() => { throw new Error('NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY is required for NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY'); })()),
     (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? (() => { throw new Error('NEXT_PUBLIC_SUPABASE_ANON_KEY is required for NEXT_PUBLIC_SUPABASE_ANON_KEY'); })())
   );
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -5,6 +5,7 @@ export const createClient = async () => {
   const cookieStore = await cookies();
   return createServerClient(
     (process.env.NEXT_PUBLIC_SUPABASE_URL ?? (() => { throw new Error('NEXT_PUBLIC_SUPABASE_URL is required for NEXT_PUBLIC_SUPABASE_URL'); })()),
+    (process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ?? (() => { throw new Error('NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY is required for NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY'); })()),
     (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? (() => { throw new Error('NEXT_PUBLIC_SUPABASE_ANON_KEY is required for NEXT_PUBLIC_SUPABASE_ANON_KEY'); })()),
     {
       cookies: {
